48082464a6a0e887e6a436aab34f5303fdd6f047
Kanan Bakirli <kanan.bk@gmail.com>
2022-04-09 20:30:19 -0400

    training model

---
+++ train.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from get_loader import dataset, data_loader
+from model import EncoderDecoder
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+#Hyperparams
+embed_size=300
+vocab_size = len(dataset.vocab)
+attention_dim=256
+encoder_dim=2048
+decoder_dim=512
+learning_rate = 3e-4
+
+num_epochs = 20
+print_every = 100
+
+#init model
+model = EncoderDecoder(
+    embed_size=300,
+    vocab_size = len(dataset.vocab),
+    attention_dim=256,
+    encoder_dim=2048,
+    decoder_dim=512
+).to(device)
+
+criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+def save_model(model,num_epochs):
+    model_state = {
+        'num_epochs':num_epochs,
+        'embed_size':embed_size,
+        'vocab_size':len(dataset.vocab),
+        'attention_dim':attention_dim,
+        'encoder_dim':encoder_dim,
+        'decoder_dim':decoder_dim,
+        'state_dict':model.state_dict()
+    }
+
+    torch.save(model_state,'attention_model_state.pth')
+    
+
+for epoch in range(1,num_epochs+1):   
+    for idx, (image, captions) in enumerate(iter(data_loader)):
+        image,captions = image.to(device),captions.to(device)
+
+        # Zero the gradients.
+        optimizer.zero_grad()
+
+        # Feed forward
+        outputs,attentions = model(image, captions)
+
+        # Calculate the batch loss.
+        targets = captions[:,1:]
+        loss = criterion(outputs.view(-1, vocab_size), targets.reshape(-1))
+        
+        # Backward pass.
+        loss.backward()
+
+        # Update the parameters in the optimizer.
+        optimizer.step()
+
+        if (idx+1)%print_every == 0:
+            print("Epoch: {} loss: {:.5f}".format(epoch,loss.item()))
+            
+            
+            #generate the caption
+            model.eval()
+            with torch.no_grad():
+                dataiter = iter(data_loader)
+                img,_ = next(dataiter)
+                features = model.encoder(img[0:1].to(device))
+                caps,alphas = model.decoder.generate_caption(features,vocab=dataset.vocab)
+                caption = ' '.join(caps)
+                                
+            model.train()
+        
+    #save the latest model
+    save_model(model,epoch)
