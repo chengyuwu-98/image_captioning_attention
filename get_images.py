127d80b10fa9f103ca946ff1d62c55b4ead9ebc6
Liu Yudong <87921304+1999Lyd@users.noreply.github.com>
2022-04-12 01:45:25 +0800

    Update get_images.py

--- get_images.py
+++ get_images.py
@@ -3,38 +3,36 @@ from bs4 import BeautifulSoup
 import os
 
 
-def get_path(url):
-    return "static/URL_" + str(url.replace("/", "_"))
+def get_directory(url):
+    return "URL_" + str(url.replace("/","_"))
 
+def get_path(url):
+    return "static/URL_" + str(url.replace("/","_"))
 
 headers = {
     'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
-}
+    }
 
 def get_images(url):
-   # get the directory path
     path = get_path(url)
+
     try:
         os.mkdir(path)
     except:
         pass
-    # request the source code from the URL
     response = requests.request("GET", url, headers=headers)
-    # parse the data through the Beautiful Soup
     data = BeautifulSoup(response.text, 'html.parser')
-    # find the image tag in the source code
     images = data.find_all('img', src=True)
-    # extract the source from all the image tags
+    print('Number of Images: ', len(images))
+    # select src tag
     image_src = [x['src'] for x in images]
-    # select only jpeg format images
+    # select only jp format images
     image_src = [x for x in image_src if x.endswith('.jpeg') ]
     image_count = 1
-    # store the image in the specified directory
     for image in image_src:
         print(image)
         image_file_name = path+'/'+str(image_count)+'.jpeg'
         print(image_file_name)
-        # open the file in write binary form and add the image content to store it
         with open(image_file_name, 'wb') as f:
             res = requests.get(image)
             f.write(res.content)
